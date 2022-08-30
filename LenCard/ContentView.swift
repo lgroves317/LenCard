4c77177712f75605ebe66e85bd318b1a44caa3bc
Len Groves <lgroves317@gmail.com>
2022-08-30 12:18:18 -0400

    Initial Commit

---
+++ LenCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LenCard
+//
+//  Created by Leonard Groves on 8/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LenCard/LenCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenCardApp.swift
+//  LenCard
+//
+//  Created by Leonard Groves on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LenCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
